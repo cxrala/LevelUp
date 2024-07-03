bf9472dc5547c2034dc9591096fc46d75162dd15
Ducquwee <121057573+Ducquwee@users.noreply.github.com>
2024-07-03 15:26:02 +0100

    Added method to find all objects of type Country

--- Assets/Scripts/Map/Country.cs
+++ Assets/Scripts/Map/Country.cs
@@ -3,16 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using UnityEngine;
+using System.ComponentModel;
 
 public class Country : MonoBehaviour
 {
     [SerializeField] private string countryName;
-    [SerializeField] private float countryPopulation;
+    [SerializeField] private int countryPopulation;
 
-    [SerializeField] private float technology;
-    [SerializeField] private float aggressiveness;
-    [SerializeField] private float hunger;
-    [SerializeField] private float fertility;
+    [SerializeField, Range(0f, 1f)] private float technology;
+    [SerializeField, Range(0f, 1f)] private float aggressiveness;
+    [SerializeField, Range(0f, 1f)] private float hunger;
+    [SerializeField, Range(0f, 1f)] private float fertility;
 
+    [SerializeField] private Country[] countryList;
     [SerializeField] private List<Country> connectingCountries;
+    
+    private void Start() {
+        countryList = FindObjectsOfType<Country>();
+    }
+
+
 }

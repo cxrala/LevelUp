c62008f7b1480627031869f2ead881a7f69ec1f6
Ducquwee <ducquwee@gmail.com>
2024-07-06 19:26:45 +0100

    Added camera controller
    Camera confined to area, needs game object to be confined

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject cameraGameObject;
+    [SerializeField] private GameObject cameraTarget;
+    public float moveSpeed = 5f;
+
+    void Update() {
+        MoveCamera();
+    }
+
+    void MoveCamera() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+    
+        Vector3 move = moveSpeed * Time.deltaTime * new Vector3(h, v, 0);
+        cameraTarget.transform.position += move;
+        //need to fix camera target moving past boundary 
+    }
+}

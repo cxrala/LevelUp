c15b35e09d274d34aaf16033794c01bea766f45e
kwan wynn <60003997+mintkw@users.noreply.github.com>
2024-07-07 12:13:15 +0100

    Random map generation (for testing) and dynamic grid scaling

--- Assets/Scripts/Map/CountryTilemap.cs
+++ Assets/Scripts/Map/CountryTilemap.cs
@@ -7,8 +7,6 @@ public class CountryTilemap : MonoBehaviour
 {
     private Tilemap tilemap; // the tilemap component of this country
     [SerializeField] private Tile fillTile; // tile to fill the country's tilemap with
-    
-    private Vector3Int tilemapSize; // size of the tilemap
 
     // the following probably should not be public but this is the life we live
     public int countryID; // the id representing this country in the map
@@ -22,42 +20,23 @@ public class CountryTilemap : MonoBehaviour
     {
         tilemap = GetComponent<Tilemap>();
 
-        // get bounds
-        tilemapSize = tilemap.cellBounds.size;
-        Vector3Int minCoord = tilemap.cellBounds.min;
-        Vector3Int maxCoord = tilemap.cellBounds.max;
-
-        // adjust bounds and size to keep within a certain window, with margins of 1/8 the relevant dimension
-        // tilemapSize.x -= Mathf.RoundToInt(tilemapSize.x / 4);
-        // tilemapSize.y -= Mathf.RoundToInt(tilemapSize.y / 4);
-        // minCoord.x -= Mathf.RoundToInt(minCoord.x / 4);
-        // minCoord.y -= Mathf.RoundToInt(minCoord.y / 4);
-
-        Debug.Log($"size: {tilemapSize}, min coord: {minCoord}, max coord: {maxCoord}");
-
-        if (MapDisplayManager == null) {
-            Debug.Log("no map display manager attached");
-        }
         List<List<int>> map = MapDisplayManager.GetComponent<MapDisplayManager>().GetMap();
+        Vector3Int startCoord = MapDisplayManager.GetComponent<MapDisplayManager>().GetStartCoord();
+
+        Debug.Log($"start coordinate: {startCoord}");
 
         // iterate over each tile and colour it 
         for (int i = 0; i < map.Count; i++) {
             for (int j = 0; j < map[0].Count; j++) {
                 if (map[i][j] == countryID) {
-                    int x = minCoord.x + j;
-                    int y = maxCoord.y - i - 1; // workaround for now to stop it going out of bounds
+                    int x = startCoord.x + j;
+                    int y = startCoord.y - i;
 
                     fillCell(new Vector3Int(x, y, 0), countryColour);
                     // Debug.Log($"filled cell at {x}, {y}");
                 }
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     // fill the cell at `position` with a tile of solid colour `color`
--- Assets/Scripts/Map/MapDisplayManager.cs
+++ Assets/Scripts/Map/MapDisplayManager.cs
@@ -5,23 +5,34 @@ using UnityEngine;
 public class MapDisplayManager : MonoBehaviour
 {
     private List<List<int>> map; // grid of cells containing country IDs, type int for now
+    private int scaleFactor; // scale factor for the map
+
     private Dictionary<int, GameObject> countries = new Dictionary<int, GameObject>(); // map from country ID to game object
 
     [SerializeField] private GameObject countryTilemap; // tilemap prefab to be instantiated for each country
     [SerializeField] private GameObject grid; // grid object, parent to all tilemaps
 
+    private Vector2Int gridSize; // shape of the grid in cells
+    private Vector3Int startCoord; // coordinate of the top left cell in the grid
+
     // Start is called before the first frame update
     void Awake()
     {
         // to delete: initialise map
-        map = new List<List<int>>();
-        map.Add(new List<int>{0, 1, 1, 0, 5, 5, 0});
-        map.Add(new List<int>{1, 1, 1, 0, 5, 5, 5});
-        map.Add(new List<int>{1, 1, 1, 0, 3, 3, 5});
-        map.Add(new List<int>{0, 0, 0, 0, 3, 3, 0});
-        map.Add(new List<int>{0, 0, 2, 2, 2, 3, 0});
-        map.Add(new List<int>{0, 0, 0, 2, 2, 3, 0});
-        map.Add(new List<int>{0, 0, 0, 0, 0, 3, 0});
+        // map = new List<List<int>>();
+        // map.Add(new List<int>{0, 1, 1, 0, 5, 5, 0});
+        // map.Add(new List<int>{1, 1, 1, 0, 5, 5, 5});
+        // map.Add(new List<int>{1, 1, 1, 0, 3, 3, 5});
+        // map.Add(new List<int>{0, 0, 0, 0, 3, 3, 0});
+        // map.Add(new List<int>{0, 0, 2, 2, 2, 3, 0});
+        // map.Add(new List<int>{0, 0, 0, 2, 2, 3, 0});
+        // map.Add(new List<int>{0, 0, 0, 0, 0, 3, 0});
+
+        // to delete: randomly initialise map
+        GenerateRandomMap();
+
+        // adjust grid based on shape of input
+        AdjustGrid();
 
         // loop over map cells
         foreach (List<int> row in map) {
@@ -48,6 +59,25 @@ public class MapDisplayManager : MonoBehaviour
             i++;
         }
     }
+
+    // to delete: generate random noise as a map for testing purposes
+    void GenerateRandomMap() {
+        scaleFactor = Random.Range(10, 50); // change to 10, 100 later
+        // scaleFactor = 1;
+        map = new List<List<int>>();
+
+        for (int row = 0; row < scaleFactor * 9; row++) {
+            List<int> newRow = new List<int>();
+
+            for (int col = 0; col < scaleFactor * 16; col++) {
+                newRow.Add(Random.Range(0, 5));
+            }
+
+            map.Add(newRow);
+        }
+
+        gridSize = new Vector2Int(map[0].Count, map.Count);
+    }
     
     // generates k random colours that look (more or less) good together - totally stole the idea off this btw
     // https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
@@ -66,12 +96,27 @@ public class MapDisplayManager : MonoBehaviour
         return colours;
     }
 
-    // resize grid cell size based on map size
+    // resize grid cell size based on map size. always 16:9 ratio
     void AdjustGrid() {
-        // by default, scale of (1, 1) yields a 10x18 grid
+        // by default, scale of (1, 1) yields a 10x18 grid i think
+        float newScale = 1f / scaleFactor; // yields a non exact fit but reduces chance of going out of frame
+
+        // may scrap this: cap the in-screen cell resolution so cells don't get too small?
+        newScale = Mathf.Max(newScale, 0.1f);
+
+        grid.transform.localScale = new Vector3(newScale, newScale, 0);
+
+        // calculate start coordinate
+        startCoord = new Vector3Int(0, 0, 0);
+        startCoord.x = (int) -Mathf.Floor(gridSize.x / 2);
+        startCoord.y = (int) Mathf.Floor(gridSize.y / 2);
     }
 
     public List<List<int>> GetMap() {
         return map;
+    }
+
+    public Vector3Int GetStartCoord() {
+        return startCoord;
     }
 }
